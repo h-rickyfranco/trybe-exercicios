cbad65f38c1fe2b88955eaa612d4ee42558dc6f0
Henrique Franco <hrick_gtr@hotmail.com>
2023-05-03 11:08:05 -0300

    front-end 2.1

---
+++ front-end/secao-2-introducao-ao-react/dia-01/primeiro-exercicio-react/src/About.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+class About extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>
+                    Henrique Franco
+                </h1>
+                <p>
+                    Músico, apaixonado por produção musical e matemática! Estudante de desenvolvimento web!
+                </p>
+                <h2>
+                    Minhas Habilidades
+                </h2>
+                <ul>
+                    <li>Tocar guitarra</li>
+                    <li>Beber café</li>
+                    <li>Dormir</li>
+                </ul>
+            </div>
+        );
+    }
+}
+
+export default About;
